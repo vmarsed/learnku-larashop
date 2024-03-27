8729d7481ecd963f2b528f5f30adc1710e284ebd
onion <onion@vege.com>
2024-03-27 20:53:57 +0800

    后台控制器

--- app/Admin/Controllers/UsersController.php
+++ app/Admin/Controllers/UsersController.php
@@ -15,10 +15,12 @@ class UsersController extends AdminController
      *
      * @var string
      */
-    protected $title = 'User';
+    protected $title = '用户';
 
     /**
      * Make a grid builder.
+     * grid() 方法来决定列表页要展示哪些列，以及各个字段对应的名称
+     * Laravel-Admin 会通过读取数据库自动把所有的字段都列出来
      *
      * @return Grid
      */
@@ -37,11 +39,27 @@ class UsersController extends AdminController
         $grid->column('created_at', __('Created at'));
         $grid->column('updated_at', __('Updated at'));
 
+        // 不在页面显示 `新建` 按钮，因为我们不需要在后台新建用户
+        $grid->disableCreateButton();
+        // 同时在每一行也不显示 `编辑` 按钮
+        $grid->disableActions();
+
+        $grid->tools(function ($tools) {
+            // 禁用批量删除按钮
+            $tools->batch(function ($batch) {
+                $batch->disableDelete();
+            });
+        });
+
+
         return $grid;
     }
 
     /**
      * Make a show builder.
+     * detail() 方法用来展示用户详情页
+     * 通过调用 detail() 方法来决定要展示哪些字段
+     * Laravel-Admin 默认会通过读取数据库自动把所有的字段都列出来
      *
      * @param mixed $id
      * @return Show
@@ -63,7 +81,8 @@ class UsersController extends AdminController
     }
 
     /**
-     * Make a form builder.
+     * 教程删除这个方法
+     * form() 方法用于编辑和创建用户，由于我们不会在管理后台去新增和编辑用户，所以可以把这个方法删除。
      *
      * @return Form
      */
