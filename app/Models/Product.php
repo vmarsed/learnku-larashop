74deb2dac41ee6ad772248bfc1fb956a50ea8cc3
onion <onion@vege.com>
2024-03-29 14:27:51 +0800

    前台图片地址直接用数据库里的地址是错的, 在产品模型添加访问器, 访问image时,自动修改链接

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -25,7 +25,7 @@ class Product extends Model
     public function getImageUrlAttribute()
     {
         // 如果 image 字段本身就已经是完整的 url 就直接返回
-        if (Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
+        if (\Str::startsWith($this->attributes['image'], ['http://', 'https://'])) {
             return $this->attributes['image'];
         }
         return \Storage::disk('public')->url($this->attributes['image']);
