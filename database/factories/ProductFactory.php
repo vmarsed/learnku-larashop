90ae4b41aae248e0058f1515b432b790b4782965
onion <onion@vege.com>
2024-03-29 15:23:23 +0800

    Product工厂改图源,教程图源防盗了,在前端显示不出来

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -19,16 +19,17 @@ class ProductFactory extends Factory
     {
 
         $image = $this->faker->randomElement([
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/7kG1HekGK6.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/1B3n0ATKrn.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/r3BNRe4zXG.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/C0bVuKB2nt.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/82Wf2sg8gM.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/nIvBAQO5Pj.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/XrtIwzrxj7.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/uYEHCJ1oRp.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/2JMRaFwRpo.jpg",
-            "https://cdn.learnku.com/uploads/images/201806/01/5320/pa7DrV43Mw.jpg",
+            "https://images.unsplash.com/photo-1557180295-76eee20ae8aa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8b2JqZWN0fGVufDB8fDB8fHww",
+            "https://images.unsplash.com/photo-1459411552884-841db9b3cc2a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8b2JqZWN0fGVufDB8fDB8fHww",
+            "https://images.unsplash.com/photo-1563219996-45f1a0ba692e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8b2JqZWN0fGVufDB8fDB8fHww",
+            "https://images.unsplash.com/photo-1494232410401-ad00d5433cfa?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fG9iamVjdHxlbnwwfHwwfHx8MA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1686917087297-97ef5ee6e9b3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fG9iamVjdHxlbnwwfHwwfHx8MA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1678579267285-d562dc53e783?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjF8fG9iamVjdHxlbnwwfHwwfHx8MA%3D%3D",
+            "https://images.unsplash.com/photo-1598300042247-d088f8ab3a91?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fG9iamVjdHxlbnwwfHwwfHx8MA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1679513691641-9aedddc94f96?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDV8fG9iamVjdHxlbnwwfHwwfHx8MA%3D%3D",
+            "https://images.unsplash.com/photo-1489824904134-891ab64532f1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTl8fG9iamVjdHxlbnwwfHwwfHx8MA%3D%3D",
+            "https://plus.unsplash.com/premium_photo-1667506422352-32aec976293a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjF8fG9iamVjdHxlbnwwfHwwfHx8MA%3D%3D",
+            "https://images.unsplash.com/photo-1501426026826-31c667bdf23d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTAwfHxvYmplY3R8ZW58MHx8MHx8fDA%3D",
         ]);
 
         return [
