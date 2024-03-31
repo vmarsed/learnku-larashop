5d828707b5a1baf6c7f6dc291a4eca5e28a52cce
onion <onion@vege.com>
2024-03-31 13:26:30 +0800

    6.4 购物车页的下单表单

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -40,8 +40,12 @@ class CartController extends Controller
     {
         // with(['productSku.product']) 方法用来预加载购物车里的商品和 SKU 信息
         $cartItems = $request->user()->cartItems()->with(['productSku.product'])->get();
+        $addresses = $request->user()->addresses()->orderBy('last_used_at', 'desc')->get();
 
-        return view('cart.index', ['cartItems' => $cartItems]);
+        return view('cart.index', [
+            'cartItems' => $cartItems,
+            'addresses' => $addresses,
+        ]);
     }
 
 
