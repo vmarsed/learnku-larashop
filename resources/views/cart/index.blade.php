fadffe828c8a30986606adc334cc239eeee470f7
onion <onion@vege.com>
2024-04-01 17:58:00 +0800

    购物车页 > 下单成功 > JS > 跳转到 > 用户订单列表

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -174,7 +174,10 @@
       axios.post('{{ route('orders.store') }}', req)
         .then(
           function (response) {
-            swal('订单提交成功', '', 'success');
+            swal('订单提交成功', '', 'success')
+            .then(()=>{
+              location.href = '/orders/' + response.data.id;
+            });
           }, 
           function (error) {
             if (error.response.status === 422) {
