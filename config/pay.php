1ae491740e0365e0b50f816d3eaea1693db79885
onion <onion@vege.com>
2024-04-02 20:54:04 +0800

    yansongda/pay alipay test

--- config/pay.php
+++ config/pay.php
@@ -1,39 +1,107 @@
 <?php
-use Yansongda\Pay\Pay;
 
+use Yansongda\Pay\Pay;
 return [
     'alipay' => [
-        'app_id'         => '9021000135683039',
-        # 支付宝公钥
-        'ali_public_key' => 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvHAze8JsGC3+BYzplrQPP6dU3b+20NLh5DzbC0m5m5S+/3cjNOo802nMnMyGkkcuRfc2VNJd5epN5X1p/baJPv9GcPkbHOEEFj3FiKi85zoGHdBCYr+OkPshEQ7eTnCt84SKTeZeuBf7RmBqxmneL8qGpbI19+c91Xg3ni0MQMrNTll3ffBSzRXL6KWrotT1JC4I1BUK0uYcPFzME+irlfm/fLcYFyb9eOhUImDI6K9Dl14RFJIHhUlAEWWScnki+4jtolFugU54ZoegVq9phtq0NMbaMVuuXHwRAnreg9OOt+7Qkpt3z2NnzteNaNiCIzjQu69+NQSmTyTRA5h0ywIDAQAB',
-        # 应用私钥
-        'private_key'    => 'MIIEogIBAAKCAQEAydsKuqzCl1//2UUlGWOadeAAddr3TBIwIpuz+2BU7+dyFGz/c80jxmyi16QbDuOk7n64xFhiU83zyAvYsqnsYEm4fbRPkgl89WDRjYzWt5iZSTSwa+3RpL875w363cYxd+n+HKcQDMGPCNpk5PRiKnveX3jcPb/P/9Erp2WdMzGXuJZnHuS9xj7XWp79Y774VAyEaMNZV35Q+hc4cQg/NBGL5cSfYDdUwb41hIFtntoG34iNiNfixsXgo77WVr0cEwA56VHndHO8wzHqIePbcwsokUK6WhCb0boQ+b3JF/mwfn0fwxYI3PQa45PJ6dK4GRwTpcDdrVA7tP21EmGpyQIDAQABAoIBAAoHT+kmXCSZKGTrAoTYZfwlPOCeGFs3LB0MD00V5CJnJUvOfoKKVpStIQoFpTKALNmMgjF/EqOHP+1YaVO3DX9bKkdUhf+DaSnnvYGC1cgZVMRGUd2K2zeMKLW6BBSwnBSRE4bo5es8UpbmAs09NwNVVB76KjcHKJZVFg4jBkRcw81isCp9euTLvB2TUsugE2efIHBtoKA5MTmTc2/dqVx2V8u4PrP5jU5q/9ieSEOSwEsg5NDyxuPxpRavftOg3+kQHqimUTVPxPwLTAazGkdgtNV6/VLFKDMbF9lhWa0ooTfL44PMbvlCszJ5JZSI6RTJxorD/0D2nfmvmkTh/uECgYEA57iBi85LEzk8QB3I+4ZH1QGFBhRareweB8OHc02qGolOimivwCWUwVGmYiQSUHhmZ7S5acPxbED7LO7eoB/hxxK6cKA9sGPcg5My1QapOrDy1r4AoCDLzRj1aawCAM752d5WFUIyT8OvRtA98kxPM6ulhkD2zXOKomOz/xGQAuMCgYEA3wF1SPs+CfqQCt6H+6hnfUrIRmJmEh088tLQKYQlb/qqxPFxJLcn0lboyahJThDULr6HcFyE+1c0SqOIWTE4JQuXk8K2WV/EVJA1zJ1/vX8v4+TiRm3ovbfhWtgEgVOCy+Yyu0ZltOCHJrIhAK6dflGQFEUj2b4HGPqtaOnXBGMCgYBac20uE5K+qELajEXd2ObFQNiaLOvQpDB8u1huK8zQmvRrlr2z9XeBlsup14bGnpjqtmMB5BijNyJUwdFpE8jgGnFocURjTDfRrG3VNcptPJEuPfCkkOi84Jc7bodBpYlmACH+Hl7hP/N1YugrYpowAzqTk1xDimx9QwoJ8OMUHQKBgBVxsHOKJVcUTxGcgHsEoJ79t/B8uc/4G8pF11qGqIryEcXKadTb/+pMDSqHEjV9jMnJ7IAzhV4d7ptWyl+5SerWCIde6+YAPYLcMzAv3P694j8J3XBkVUMZEYSIzTYab7NnuW0kwdb42EYyTtYf9GkXMsGgPGhNy6TtRguWoOR9AoGAZPDZEsBYhN91lrj1PPeOOkKYm+qBJCKqPRJa7GaJXip2AsdAisBsSwwdsP88vvgAfMAGsSBAVOs0y/hR2gkXdlPGBVr7L1rX9DVL3xmYz0Q8QEzGghwfT8/8gegfkQwcYSdFFyJNLNcJpq67TbXBSqyO2ux91+ofM9KlxSFrX10=',
-        # 应用私钥
-        // 'app_secret_cert' => 'MIIEogIBAAKCAQEAydsKuqzCl1//2UUlGWOadeAAddr3TBIwIpuz+2BU7+dyFGz/c80jxmyi16QbDuOk7n64xFhiU83zyAvYsqnsYEm4fbRPkgl89WDRjYzWt5iZSTSwa+3RpL875w363cYxd+n+HKcQDMGPCNpk5PRiKnveX3jcPb/P/9Erp2WdMzGXuJZnHuS9xj7XWp79Y774VAyEaMNZV35Q+hc4cQg/NBGL5cSfYDdUwb41hIFtntoG34iNiNfixsXgo77WVr0cEwA56VHndHO8wzHqIePbcwsokUK6WhCb0boQ+b3JF/mwfn0fwxYI3PQa45PJ6dK4GRwTpcDdrVA7tP21EmGpyQIDAQABAoIBAAoHT+kmXCSZKGTrAoTYZfwlPOCeGFs3LB0MD00V5CJnJUvOfoKKVpStIQoFpTKALNmMgjF/EqOHP+1YaVO3DX9bKkdUhf+DaSnnvYGC1cgZVMRGUd2K2zeMKLW6BBSwnBSRE4bo5es8UpbmAs09NwNVVB76KjcHKJZVFg4jBkRcw81isCp9euTLvB2TUsugE2efIHBtoKA5MTmTc2/dqVx2V8u4PrP5jU5q/9ieSEOSwEsg5NDyxuPxpRavftOg3+kQHqimUTVPxPwLTAazGkdgtNV6/VLFKDMbF9lhWa0ooTfL44PMbvlCszJ5JZSI6RTJxorD/0D2nfmvmkTh/uECgYEA57iBi85LEzk8QB3I+4ZH1QGFBhRareweB8OHc02qGolOimivwCWUwVGmYiQSUHhmZ7S5acPxbED7LO7eoB/hxxK6cKA9sGPcg5My1QapOrDy1r4AoCDLzRj1aawCAM752d5WFUIyT8OvRtA98kxPM6ulhkD2zXOKomOz/xGQAuMCgYEA3wF1SPs+CfqQCt6H+6hnfUrIRmJmEh088tLQKYQlb/qqxPFxJLcn0lboyahJThDULr6HcFyE+1c0SqOIWTE4JQuXk8K2WV/EVJA1zJ1/vX8v4+TiRm3ovbfhWtgEgVOCy+Yyu0ZltOCHJrIhAK6dflGQFEUj2b4HGPqtaOnXBGMCgYBac20uE5K+qELajEXd2ObFQNiaLOvQpDB8u1huK8zQmvRrlr2z9XeBlsup14bGnpjqtmMB5BijNyJUwdFpE8jgGnFocURjTDfRrG3VNcptPJEuPfCkkOi84Jc7bodBpYlmACH+Hl7hP/N1YugrYpowAzqTk1xDimx9QwoJ8OMUHQKBgBVxsHOKJVcUTxGcgHsEoJ79t/B8uc/4G8pF11qGqIryEcXKadTb/+pMDSqHEjV9jMnJ7IAzhV4d7ptWyl+5SerWCIde6+YAPYLcMzAv3P694j8J3XBkVUMZEYSIzTYab7NnuW0kwdb42EYyTtYf9GkXMsGgPGhNy6TtRguWoOR9AoGAZPDZEsBYhN91lrj1PPeOOkKYm+qBJCKqPRJa7GaJXip2AsdAisBsSwwdsP88vvgAfMAGsSBAVOs0y/hR2gkXdlPGBVr7L1rX9DVL3xmYz0Q8QEzGghwfT8/8gegfkQwcYSdFFyJNLNcJpq67TbXBSqyO2ux91+ofM9KlxSFrX10=',
-        # 应用公钥证书
-        // 'app_public_cert_path' => base_path('secret/appPublicCert.crt'),
-        # 支付宝公钥证书
-        // 'alipay_public_cert_path' => base_path('secret/alipayPublicCert.crt'),
-        # 支付宝根证书
-        // 'alipay_root_cert_path' => base_path('secret/alipayRootCert.crt'),
-        // 'return_url' => 'http://larashop.wodetou.top/alipay',
-        // 'notify_url' => 'http://larashop.wodetou.top/alipay',
-        // 'app_auth_token' => '', // 选填 - 第三方应用授权 token
-        // 'service_provider_id' => '', //选填-服务商模式下的服务商 id , 当 mode 为 Pay::MODE_SERVICE 时使用参数
-        // 'mode' => Pay::MODE_NORMAL, // 选填 - 默认为正常模式. 可选为: MODE_NORMAL, MODE_SANDBOX , MODE_SERVICE
-        'log'            => [
-            'file' => storage_path('logs/alipay.log'),
-        ],
+        'default' => [
+            // 必填-支付宝分配的 app_id
+            'app_id' => '9021000135683039',
+            // 必填-应用私钥 字符串或路径
+            // 在 https://open.alipay.com/develop/manage 《应用详情->开发设置->接口加签方式》中设置
+            'app_secret_cert' => 'MIIEogIBAAKCAQEAydsKuqzCl1//2UUlGWOadeAAddr3TBIwIpuz+2BU7+dyFGz/c80jxmyi16QbDuOk7n64xFhiU83zyAvYsqnsYEm4fbRPkgl89WDRjYzWt5iZSTSwa+3RpL875w363cYxd+n+HKcQDMGPCNpk5PRiKnveX3jcPb/P/9Erp2WdMzGXuJZnHuS9xj7XWp79Y774VAyEaMNZV35Q+hc4cQg/NBGL5cSfYDdUwb41hIFtntoG34iNiNfixsXgo77WVr0cEwA56VHndHO8wzHqIePbcwsokUK6WhCb0boQ+b3JF/mwfn0fwxYI3PQa45PJ6dK4GRwTpcDdrVA7tP21EmGpyQIDAQABAoIBAAoHT+kmXCSZKGTrAoTYZfwlPOCeGFs3LB0MD00V5CJnJUvOfoKKVpStIQoFpTKALNmMgjF/EqOHP+1YaVO3DX9bKkdUhf+DaSnnvYGC1cgZVMRGUd2K2zeMKLW6BBSwnBSRE4bo5es8UpbmAs09NwNVVB76KjcHKJZVFg4jBkRcw81isCp9euTLvB2TUsugE2efIHBtoKA5MTmTc2/dqVx2V8u4PrP5jU5q/9ieSEOSwEsg5NDyxuPxpRavftOg3+kQHqimUTVPxPwLTAazGkdgtNV6/VLFKDMbF9lhWa0ooTfL44PMbvlCszJ5JZSI6RTJxorD/0D2nfmvmkTh/uECgYEA57iBi85LEzk8QB3I+4ZH1QGFBhRareweB8OHc02qGolOimivwCWUwVGmYiQSUHhmZ7S5acPxbED7LO7eoB/hxxK6cKA9sGPcg5My1QapOrDy1r4AoCDLzRj1aawCAM752d5WFUIyT8OvRtA98kxPM6ulhkD2zXOKomOz/xGQAuMCgYEA3wF1SPs+CfqQCt6H+6hnfUrIRmJmEh088tLQKYQlb/qqxPFxJLcn0lboyahJThDULr6HcFyE+1c0SqOIWTE4JQuXk8K2WV/EVJA1zJ1/vX8v4+TiRm3ovbfhWtgEgVOCy+Yyu0ZltOCHJrIhAK6dflGQFEUj2b4HGPqtaOnXBGMCgYBac20uE5K+qELajEXd2ObFQNiaLOvQpDB8u1huK8zQmvRrlr2z9XeBlsup14bGnpjqtmMB5BijNyJUwdFpE8jgGnFocURjTDfRrG3VNcptPJEuPfCkkOi84Jc7bodBpYlmACH+Hl7hP/N1YugrYpowAzqTk1xDimx9QwoJ8OMUHQKBgBVxsHOKJVcUTxGcgHsEoJ79t/B8uc/4G8pF11qGqIryEcXKadTb/+pMDSqHEjV9jMnJ7IAzhV4d7ptWyl+5SerWCIde6+YAPYLcMzAv3P694j8J3XBkVUMZEYSIzTYab7NnuW0kwdb42EYyTtYf9GkXMsGgPGhNy6TtRguWoOR9AoGAZPDZEsBYhN91lrj1PPeOOkKYm+qBJCKqPRJa7GaJXip2AsdAisBsSwwdsP88vvgAfMAGsSBAVOs0y/hR2gkXdlPGBVr7L1rX9DVL3xmYz0Q8QEzGghwfT8/8gegfkQwcYSdFFyJNLNcJpq67TbXBSqyO2ux91+ofM9KlxSFrX10=',
+            // 必填-应用公钥证书 路径
+            // 设置应用私钥后，即可下载得到以下3个证书
+            'app_public_cert_path' => base_path('safe/alipay-sandbox/appPublicCert.crt'),
+            // 必填-支付宝公钥证书 路径
+            'alipay_public_cert_path' => base_path('safe/alipay-sandbox/alipayPublicCert.crt'),
+            // 必填-支付宝根证书 路径
+            'alipay_root_cert_path' => base_path('safe/alipay-sandbox/alipayRootCert.crt'),
+            // 'return_url' => 'xxx/return',
+            // 'notify_url' => 'xxx/notify',
+            // 选填-第三方应用授权token
+            'app_auth_token' => '',
+            // 选填-服务商模式下的服务商 id，当 mode 为 Pay::MODE_SERVICE 时使用该参数
+            'service_provider_id' => '',
+            // 选填-默认为正常模式。可选为： MODE_NORMAL, MODE_SANDBOX, MODE_SERVICE
+            'mode' => Pay::MODE_NORMAL,
+        ]
     ],
-
+    // 由于还没做到教程中对接微信的章节，所以先留空了
     'wechat' => [
-        'app_id'      => '',
-        'mch_id'      => '',
-        'key'         => '',
-        'cert_client' => '',
-        'cert_key'    => '',
-        'log'         => [
-            'file' => storage_path('logs/wechat_pay.log'),
-        ],
+        'default' => [
+            // 必填-商户号，服务商模式下为服务商商户号
+            // 可在 https://pay.weixin.qq.com/ 账户中心->商户信息 查看
+            'mch_id' => 'xxx',
+            // 选填-v2商户私钥
+            'mch_secret_key_v2' => '',
+            // 必填-v3 商户秘钥
+            // 即 API v3 密钥(32字节，形如md5值)，可在 账户中心->API安全 中设置
+            'mch_secret_key' => '',
+            // 必填-商户私钥 字符串或路径
+            // 即 API证书 PRIVATE KEY，可在 账户中心->API安全->申请API证书 里获得
+            // 文件名形如：apiclient_key.pem
+            'mch_secret_cert' => '',
+            // 必填-商户公钥证书路径
+            // 即 API证书 CERTIFICATE，可在 账户中心->API安全->申请API证书 里获得
+            // 文件名形如：apiclient_cert.pem
+            'mch_public_cert_path' => '',
+            // 必填-微信回调url
+            // 不能有参数，如?号，空格等，否则会无法正确回调
+            'notify_url' => 'https://yansongda.cn/wechat/notify',
+            // 选填-公众号 的 app_id
+            // 可在 mp.weixin.qq.com 设置与开发->基本配置->开发者ID(AppID) 查看
+            'mp_app_id' => 'xxx',
+            // 选填-小程序 的 app_id
+            'mini_app_id' => '',
+            // 选填-app 的 app_id
+            'app_id' => '',
+            // 选填-合单 app_id
+            'combine_app_id' => '',
+            // 选填-合单商户号
+            'combine_mch_id' => '',
+            // 选填-服务商模式下，子公众号 的 app_id
+            'sub_mp_app_id' => '',
+            // 选填-服务商模式下，子 app 的 app_id
+            'sub_app_id' => '',
+            // 选填-服务商模式下，子小程序 的 app_id
+            'sub_mini_app_id' => '',
+            // 选填-服务商模式下，子商户id
+            'sub_mch_id' => '',
+            // 选填-微信平台公钥证书路径, optional，强烈建议 php-fpm 模式下配置此参数
+            'wechat_public_cert_path' => [
+                '45F59D4DABF31918AFCEC556D5D2C6E376675D57' => __DIR__.'/Cert/wechatPublicKey.crt',
+            ],
+            // 选填-默认为正常模式。可选为： MODE_NORMAL, MODE_SERVICE
+            'mode' => Pay::MODE_NORMAL,
+        ]
+    ],
+    // 银联支付
+    // 'unipay' => [
+    //     'default' => [
+    //         // 必填-商户号
+    //         'mch_id' => '',
+    //         // 必填-商户公私钥
+    //         'mch_cert_path' => __DIR__.'/Cert/unipayAppCert.pfx',
+    //         // 必填-商户公私钥密码
+    //         'mch_cert_password' => '000000',
+    //         // 必填-银联公钥证书路径
+    //         'unipay_public_cert_path' => __DIR__.'/Cert/unipayCertPublicKey.cer',
+    //         // 必填
+    //         'return_url' => 'https://yansongda.cn/unipay/return',
+    //         // 必填
+    //         'notify_url' => 'https://yansongda.cn/unipay/notify',
+    //     ],
+    // ],
+    'logger' => [
+        'enable' => true,
+        'file' => storage_path('logs/alipay.log'),
+        'level' => 'info', // 建议生产环境等级调整为 info，开发环境为 debug
+        'type' => 'daily', // optional, 可选 daily.
+        'max_file' => 30, // optional, 当 type 为 daily 时有效，默认 30 天
+    ],
+    'http' => [ // optional
+                'timeout' => 5.0,
+                'connect_timeout' => 5.0,
+                // 更多配置项请参考 [Guzzle](https://guzzle-cn.readthedocs.io/zh_CN/latest/request-options.html)
     ],
 ];
