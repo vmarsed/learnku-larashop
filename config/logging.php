d10483d5862fa012a8b25a971ccf74cc8f5e5295
onion <onion@vege.com>
2024-04-04 00:51:57 +0800

    pay 前端回调 OK 教程里的 verify 函数不行了, 改成 callback 函数了

--- config/logging.php
+++ config/logging.php
@@ -56,7 +56,7 @@ return [
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.single.log'),
+            'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
         ],
 
