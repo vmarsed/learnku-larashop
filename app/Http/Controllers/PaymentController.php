868974536e64568d49e73e75b1cbc4e51ff3489a
onion <onion@vege.com>
2024-04-04 14:17:05 +0800

    创建订单完成事件OrderPaid >> 控制器的后端回调在支付完成后触发OrderPaid事件

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -8,6 +8,9 @@ use App\Models\Order;
 use App\Exceptions\InvalidRequestException;
 
 use Carbon\Carbon;
+# U7.8 完善支付后逻辑
+use App\Events\OrderPaid;
+
 
 
 class PaymentController extends Controller
@@ -28,8 +31,6 @@ class PaymentController extends Controller
             'subject'      => '支付 Laravel Shop 的订单：'.$order->no, // 订单标题
         ]);
     }
-
-
 
     // 前端回调页面
     public function alipayReturn()
@@ -89,12 +90,16 @@ class PaymentController extends Controller
             'payment_no'     => $data->trade_no, // 支付宝订单号
         ]);
         \Log::debug('更新数据库 paid_at payment_method payment_no Order模型为',[$order]);
-
+        $this->afterPaid($order);
         return app('alipay')->success();
 
 
     }
 
+    protected function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
+    }
 
 
 
