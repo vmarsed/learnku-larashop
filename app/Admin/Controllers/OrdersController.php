dc5cf061406e0cfdfbe2092752ae89e6be7c273f
onion <onion@vege.com>
2024-04-06 07:03:56 +0800

    U8.3 在订单页发货 Fix post

--- app/Admin/Controllers/OrdersController.php
+++ app/Admin/Controllers/OrdersController.php
@@ -12,7 +12,7 @@ use Encore\Admin\Show;
 use Encore\Admin\Layout\Content;
 # U8.3 在 后台订单详情页 发货
 use Illuminate\Foundation\Validation\ValidatesRequests;
-
+use Illuminate\Foundation\Http\FormRequest as Request;
 
 
 class OrdersController extends AdminController
