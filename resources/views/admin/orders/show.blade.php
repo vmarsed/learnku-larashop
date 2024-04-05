1093acba28f7452479ca4357e9393d7118ae68d6
onion <onion@vege.com>
2024-04-05 14:27:48 +0800

    后台订单详情 Fix

--- resources/views/admin/orders/show.blade.php
+++ resources/views/admin/orders/show.blade.php
@@ -3,7 +3,7 @@
       <h3 class="box-title">订单流水号：{{ $order->no }}</h3>
       <div class="box-tools">
         <div class="btn-group float-right" style="margin-right: 10px">
-          <a href="{{ route('admin.orders.index') }}" class="btn btn-sm btn-default"><i class="fa fa-list"></i> 列表</a>
+          <a href="/admin/orders" class="btn btn-sm btn-default"><i class="fa fa-list"></i> 列表</a>
         </div>
       </div>
     </div>
