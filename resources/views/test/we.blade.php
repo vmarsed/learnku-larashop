92d5099e72c00ef7b4e22a75bc8f420764a2c8a0
onion <onion@vege.com>
2024-04-11 11:38:15 +0800

    ass

--- resources/views/test/we.blade.php
+++ resources/views/test/we.blade.php
@@ -1,6 +1,8 @@
-<form action="{{ route('test.we') }}">
+<form action="{{ route('test.we') }}" method="post">
+{{ csrf_field() }}
 platform: <input name="platform" value="wechat"/>
 auth: <input name="auth" />
 userId: <input name="usser_id" value="1" />
 <button type="submit">submit</button>
+
 </form>
