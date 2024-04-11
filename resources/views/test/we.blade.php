cdfb369f217d6c53763def6974504a8351345cf1
onion <onion@vege.com>
2024-04-11 19:52:09 +0800

    ass test

--- resources/views/test/we.blade.php
+++ resources/views/test/we.blade.php
@@ -3,7 +3,7 @@
 platform: <input name="platform" value="wechat"/>
 authKey: <input name="auth_key" value="openid" />
 authValue: <input name="auth_value" value="sdkjfowiejfsdkl" />
-userId: <input name="usser_id" value="1" />
+userId: <input name="user_id" value="1" />
 <button type="submit">submit</button>
 
 </form>
