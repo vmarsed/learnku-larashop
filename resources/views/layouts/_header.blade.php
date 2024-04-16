9f3391c1085aa87c5167521e14e7baadb600b4e9
onion <onion@vege.com>
2024-04-16 13:13:47 +0800

    目录树

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -31,16 +31,6 @@
 
 
 
-        <li class="nav-item dropdown">
-          <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </button>
-          <ul class="dropdown-menu dropdown-menu-dark">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
 
 
 
