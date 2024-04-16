ee5ab98eb8dbb45470cfe0f8f84031c3808c357f
onion <onion@vege.com>
2024-04-16 14:07:24 +0800

    目录树

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,25 +1,24 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
-    <div class="container">
+  <div class="container">
 
-        <!-- Branding Image -->
-        <a class="navbar-brand" href="{{ url('/') }}">
-            Laravel Shop
-        </a>
-
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+    <!-- Branding Image -->
+    <a class="navbar-brand " href="{{ url('/') }}">
+      Laravel Shop
+    </a>
 
 
-        <div class="navbar-collapse collapse" id="navbarSupportedContent">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
 
-            <!-- Left Side Of Navbar -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <ul class="navbar-nav mr-auto">
-                {{-- <li class="nav-item dropdown">
+    
+      <!-- Left Side Of Navbar -->
+
+      <ul class="navbar-nav mr-auto">
+        {{-- <li class="nav-item dropdown">
           <button class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
             全部分类
           </button>
@@ -28,53 +27,49 @@
           </ul>
         </li> --}}
 
-                @if (!isset($categoryTree))
-                    <li class="nav-item dropdown">
-                        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false" id="categoryTree">所有类目 <b class="caret"></b></a>
-                        <ul class="dropdown-menu" aria-labelledby="categoryTree">
-                            <!-- 遍历 $categoryTree 集合，将集合中的每一项以 $category 变量注入 layouts._category_item 模板中并渲染 -->
-                            @each('layouts._category_item', $categoryTree, 'category')
-                        </ul>
-                    </li>
-                @endif
+      @if(isset($categoryTree))
+        <li class="nav-item dropdown">
+          <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="categoryTree">所有类目 <b class="caret"></b></a>
+          <ul class="dropdown-menu" >
+            @each('layouts._category_item', $categoryTree, 'category')
+          </ul>
+        </li>
+      @endif
 
-            </ul>
+      </ul>
 
 
 
 
-            <ul class="navbar-nav navbar-right">
-                <!-- 登录注册链接开始 -->
-                @guest
-                    <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
-                    <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
-                @else
-                    <li class="nav-item">
-                        <a class="nav-link mt-1" href="{{ route('cart.index') }}"><i class="fa fa-shopping-cart"></i></a>
-                    </li>
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60"
-                                class="img-responsive img-circle" width="30px" height="30px">
-                            {{ Auth::user()->name }}
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a href="{{ route('user_addresses.index') }}" class="dropdown-item">收货地址</a>
-                            <a href="{{ route('products.favorites') }}" class="dropdown-item">我的收藏</a>
-                            <a href="{{ route('orders.index') }}" class="dropdown-item">我的订单</a>
-                            <a class="dropdown-item" id="logout" href="#"
-                                onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                {{ csrf_field() }}
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-                <!-- 登录注册链接结束 -->
-            </ul>
+      <ul class="navbar-nav navbar-right">
+        <!-- 登录注册链接开始 -->
+        @guest
+        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">登录</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">注册</a></li>
+        @else
+        <li class="nav-item">
+          <a class="nav-link mt-1" href="{{ route('cart.index') }}"><i class="fa fa-shopping-cart"></i></a>
+        </li>
+      
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <img src="https://cdn.learnku.com/uploads/images/201709/20/1/PtDKbASVcz.png?imageView2/1/w/60/h/60" class="img-responsive img-circle" width="30px" height="30px">
+            {{ Auth::user()->name }}
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a href="{{ route('user_addresses.index') }}" class="dropdown-item">收货地址</a>
+            <a href="{{ route('products.favorites') }}" class="dropdown-item">我的收藏</a>
+            <a href="{{ route('orders.index') }}" class="dropdown-item">我的订单</a>
+            <a class="dropdown-item" id="logout" href="#"
+               onclick="event.preventDefault();document.getElementById('logout-form').submit();">退出登录</a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+            </form>
+          </div>
+        </li>
+        @endguest
+        <!-- 登录注册链接结束 -->
+      </ul>
 
 
 
@@ -84,6 +79,6 @@
 
 
 
-        </div>
     </div>
+  </div>
 </nav>
