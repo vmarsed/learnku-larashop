453a1ef4f3b90f7f849461b8b4fcb21b066fd800
onion <onion@vege.com>
2024-04-16 12:48:41 +0800

    目录树

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,5 +1,32 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-static-top">
   <div class="container">
+
+
+
+      {{-- 折叠菜单开始 --}}
+      {{-- mr-auto --}}
+      <ul class="navbar-nav navbar-right">
+        <!-- 顶部类目菜单开始 -->
+        <!-- 判断模板是否有 $categoryTree 变量 -->
+        @if(isset($categoryTree))
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" role="button" aria-expanded="false" id="categoryTree">所有类目 <b class="caret"></b></a>
+            <div class="dropdown-menu" aria-labelledby="categoryTree">
+              <!-- 遍历 $categoryTree 集合，将集合中的每一项以 $category 变量注入 layouts._category_item 模板中并渲染 -->
+              {{-- @each('layouts._category_item', $categoryTree, 'category') --}}
+              <a href="#" class="dropdown-item">xxxx</a>
+            </div>
+          </li>
+        @endif
+        <!-- 顶部类目菜单结束 -->
+      </ul>
+      {{-- 折叠菜单结束 --}}
+
+
+
+
+
+
     <!-- Branding Image -->
     <a class="navbar-brand " href="{{ url('/') }}">
       Laravel Shop
@@ -33,8 +60,6 @@
         <!-- 顶部类目菜单结束 -->
       </ul>
       {{-- 折叠菜单结束 --}}
-
-
 
 
       <ul class="navbar-nav navbar-right">
